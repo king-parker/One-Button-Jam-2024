9bbb685b8d0f8be7e314fc4b9b40df2ab20bc2c7
Parker King <gnikrekrap@gmail.com>
2024-11-29 21:56:56 -0700

    Create basic level chunks

--- Assets/Scripts/Player/AngleSelector.cs
+++ Assets/Scripts/Player/AngleSelector.cs
@@ -11,17 +11,17 @@ public class AngleSelector : MonoBehaviour, ISelector
 
     private bool isIncreasing = true;
     private bool indicatorRunning = false;
-    private float initialX;
-    private float initialY;
+    private float offsetX;
+    private float offsetY;
+    //private float prevPlayerY;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.RotateAround(player.transform.position, new Vector3(0, 0, 1), minAngle);
-        initialX = transform.position.x;
-        initialY = transform.position.y;
-
-        //StopIndicator();
+        offsetX = transform.position.x - player.transform.position.x;
+        offsetY = transform.position.y - player.transform.position.y;
+        
         if (player.GetState() != PlayerController.PlayerState.AngleSelect)  { HideIndicator(); }
     }
 
@@ -61,7 +61,7 @@ public class AngleSelector : MonoBehaviour, ISelector
     [ContextMenu("Start Angle Indicator")]
     public void StartIndicator()
     {
-        transform.position = new Vector3(initialX + player.transform.position.x, initialY + player.transform.position.y, 0);
+        transform.position = new Vector3(offsetX + player.transform.position.x, offsetY + player.transform.position.y, 0);
         transform.rotation = Quaternion.AngleAxis(minAngle, Vector3.forward);
         indicatorRunning = true;
         this.gameObject.SetActive(true);
